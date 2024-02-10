9cf7648985979afcaab1a855ed7a63841fedbae9
hTaku <taku.h72@gmail.com>
2024-02-11 07:17:23 +0900

    設定ファイルを追加

--- kuma_bot.py
+++ kuma_bot.py
@@ -1,13 +1,14 @@
 import requests
-from scraping import Scraping
+from scrapings import Scraping
 from line import Line
+import settings
 
 def _convertLinePost(entry):
     return entry[0] + '\n\n' + entry[2]  + '\n\n' + '日時: ' + entry[1]
 
 if __name__ == "__main__":
     entries = Scraping().get_kama('熊')
-    line = Line('KQMwuQy8qzOs6xZ3kG3Mek8pmp9ziqs3zwv3Wb6bTk8')
+    line = Line(settings.ACCESS_TOKEN)
     if entries.__len__ != 0:
         post = _convertLinePost(entries[0])
         line.post_notify(post)
